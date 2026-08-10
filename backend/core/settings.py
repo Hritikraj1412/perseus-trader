236f3c01161ea99a4d6795476e688aa867902aec
Hritik Raj Singh <hritikrajsingh88@gmail.com>
2026-08-11 00:37:56 +0530

    Switched email backend to console to fix timeout crash

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -118,7 +118,7 @@ SIMPLE_JWT = {
 }
 
 # --- REAL SMTP EMAIL CONFIGURATION ---
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
